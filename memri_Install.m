4e499297ec13b70bc46157a22675c774378025f9
Sugocy <sugocy@gmail.com>
2026-08-18 17:46:05 +0200

    Clean up of initial processing GUI script.

--- memri_Install.m
+++ memri_Install.m
@@ -2,15 +2,24 @@
 % Install the memri-toolbox by adding the all necessary paths to pathdef.m
 % 
 % Only need to be executed before first use or if pathdef.m changes.
-% < NB. Run in root of MeMRI PTB >
+%                   < NB. Run in root of MeMRI PTB >
 
 % Add Toolbox to current-path Matlab
 fcall = dbstack('-completenames'); [fp,fn,ext] = fileparts(fcall.file);
-toolbox_directories = dir_withsub(fp);
-sdf = {toolbox_directories(:).folder};
-sdn = {toolbox_directories(:).name};
-toolbox_directories = ...
+tlbx_dirs = dir_withsub(fp); % Only directories returned.
+
+% Parent directory and childs name
+sdf = {tlbx_dirs(:).folder}; sdn = {tlbx_dirs(:).name};
+
+% Remove \.git and \ExampleData from folder
+hasGit = contains(sdf, '\.git'); sdf(hasGit) = []; sdn(hasGit) = [];
+hasExD = contains(sdf, '\_ExampleData'); sdf(hasExD) = []; sdn(hasExD) = [];
+
+% Remove \.git and \ExampleData from name
+hasGit = contains(sdn, '.git'); sdf(hasGit) = []; sdn(hasGit) = [];
+hasExD = contains(sdn, '_ExampleData'); sdf(hasExD) = []; sdn(hasExD) = [];
+
+% Combine and add paths to MATLAB's pathdef-file.
+tlbx_dirs = ...
     cellfun(@(x,y) cat(2,x,'\',y), sdf, sdn,'UniformOutput',false);
-if ~isempty(toolbox_directories)
-    addpath(toolbox_directories{:}); savepath;
-end
+if ~isempty(tlbx_dirs), addpath(tlbx_dirs{:}); savepath; end
